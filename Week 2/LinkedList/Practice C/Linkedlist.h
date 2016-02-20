de15140a2ad3fdc1bbec1d73c9c454d194931dd7
MalekSediqi <maleksediqi17@gmail.com>
2016-02-20 17:26:45 +0100

    header file veranderd

--- Week 2/LinkedList/Practice C/Linkedlist.h
+++ Week 2/LinkedList/Practice C/Linkedlist.h
@@ -1,1 +1,14 @@
-#pragma once
+#ifndef LLIST_DEF
+#define LLIST_DEF
+
+struct node
+{
+	int data;
+	struct node *next;
+};
+
+void init();
+int add(int data);
+void show();
+
+#endif
--- Week 2/LinkedList/Practice C/main.c
+++ Week 2/LinkedList/Practice C/main.c
@@ -1,7 +1,17 @@
-
-
+#include <stdio.h>
+#include "Linkedlist.h"
 
 int main(void)
 {
+	int idx;
 
+	init();
+
+	for (idx = 0; idx < 10; idx++)
+	{
+		add(idx);
+	}
+	show();
+
+	return 1;
 }
