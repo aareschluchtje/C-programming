619dae3082108a57b17e9d730be7d37726200856
MalekSediqi <maleksediqi17@gmail.com>
2016-02-21 14:57:29 +0100

    main uit de comment gehaald

--- Week 2/LinkedList/Practice C/main.c
+++ Week 2/LinkedList/Practice C/main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include "Linkedlist.h"
 
-int main(void)
+int main()
 {
 	int idx;
 
--- Week1/Week1/Opgave1-4.c
+++ Week1/Week1/Opgave1-4.c
@@ -1,20 +1,20 @@
 #include <stdio.h>
-	//void main()
-	//{
-	//	float fahr, celsius;
-	//	int laagste, hoogste, interval;
-	//	laagste = 0;
-	//	hoogste = 300;
-	//	interval = 20;
+	void main()
+	{
+		float fahr, celsius;
+		int laagste, hoogste, interval;
+		laagste = 0;
+		hoogste = 300;
+		interval = 20;
 
-	//	celsius = laagste;
-	//	printf("temperatuurconversieprogramma:n");
-	//	while( celsius <= hoogste)
-	//	{
-	//		//celsius = (5.0 / 9.0) * (fahr - 32.0);
-	//		fahr = (celsius * 9 / 5) + 32;
-	//		printf("%3.0f %6.1f\n", celsius, fahr);
-	//		celsius = celsius + interval;
-	//	}
+		celsius = laagste;
+		printf("temperatuurconversieprogramma:n");
+		while( celsius <= hoogste)
+		{
+			//celsius = (5.0 / 9.0) * (fahr - 32.0);
+			fahr = (celsius * 9 / 5) + 32;
+			printf("%3.0f %6.1f\n", celsius, fahr);
+			celsius = celsius + interval;
+		}
 
-	//}
+	}
