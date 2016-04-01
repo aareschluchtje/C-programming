016a15de290ac030504ebfb754ffdcd055abee7e
Aareschluchtje <remcosannen@gmail.com>
2016-04-01 09:12:44 +0200

    something

--- Week 2/LinkedList/Practice C/main.c
+++ Week 2/LinkedList/Practice C/main.c
@@ -1,25 +1,41 @@
 #include <stdio.h>
 #include "Linkedlist.h"
 
+char * toArray(int number);
+
 int main()
 {
-	int idx;
+	printf(toArray(13413));
+	
+	//int idx;
 
-	init();
+	//init();
 
-	for (idx = 0; idx < 10; idx++)
+	//for (idx = 0; idx < 10; idx++)
+	//{
+	//	add(idx);
+	//}
+	////remove(4);
+	////clear();
+	////printf("Element in de list : %d \n", excist(4));
+	////printf("Count linklist: %d \n", nrItems());
+	////printf("original:\n");
+	//show();
+	//deepCopy();
+	////printf("copy:\n");
+	//showCopy();
+	//getchar();
+	//return 1;
+}
+
+char * toArray(int number)
+{
+	int n = log10(number) + 1;
+	int i;
+	char *numberArray = calloc(n, sizeof(char));
+	for (i = 0; i < n; ++i, number /= 10)
 	{
-		add(idx);
+		numberArray[i] = number % 10;
 	}
-	//remove(4);
-	//clear();
-	printf("Element in de list : %d \n", excist(4));
-	printf("Count linklist: %d \n", nrItems());
-	printf("original:\n");
-	show();
-	deepCopy();
-	printf("copy:\n");
-	showCopy();
-	getchar();
-	return 1;
+	return numberArray;
 }
